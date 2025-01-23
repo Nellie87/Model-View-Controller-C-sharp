b7daae82bf1aa78bfe261dda08d0c131cba33d80
Nelius.Ndung'u <Nelius.Ndung'u@DESKTOP-F65C0VG>
2025-01-23 17:01:24 +0300

    Created the Movie Controller

---
+++ MvcStoredProcedureSample/Models/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.Entity;
+
+namespace MvcStoredProcedureSample.Models
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext() : base("name = MovieDbContext")
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().MapToStoredProcedures();
+        }
+
+    }
+}
