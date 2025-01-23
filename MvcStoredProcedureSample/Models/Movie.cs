536f774057cf4e896b25762989c70c9e52e01804
Nelius.Ndung'u <Nelius.Ndung'u@DESKTOP-F65C0VG>
2025-01-23 15:28:36 +0300

    Creating Movie Class in Models

---
+++ MvcStoredProcedureSample/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcStoredProcedureSample.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Release Date")]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public string Category { get; set; }
+    }
+}
